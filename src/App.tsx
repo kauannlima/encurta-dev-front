fb840709c80fcb14e5ab1437d23f1b29c266049a
Kauan Lima <kauanalmeidalima1405@gmail.com>
2024-10-08 23:12:47 -0300

    adicionando primeiros layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy } from '@fortawesome/free-solid-svg-icons';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const urlParaCopiar = 'http://localhost:8080/r/UFfsS';
+
+  const handleCopiarClique = () => {
+    navigator.clipboard.writeText(urlParaCopiar)
+      .then(() => {
+        alert('Texto copiado');
+      })
+      .catch((error) => {
+        console.error('Erro ao copiar o texto: ', error);
+      });
+  };
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div className='container'>
+      <h1>Encurta Dev {'{</>}'}</h1>
+      <div className="encurta-body">
+        <div className="encurta-url">
+          <input className='input-url' placeholder='Digite a URL original'></input>
+          <button className='button-url'>Encurtar URL</button>
+        </div>
+        <p className='copiar-nova-url' onClick={handleCopiarClique}>
+          <span>{urlParaCopiar}</span>
+          <span style={{ display: 'flex', alignItems: 'center' }}>
+            <FontAwesomeIcon icon={faCopy} style={{ margin: '0px 5px' }} />
+            Copiar
+          </span>
         </p>
+        <div className="encurta-qrcode">
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
