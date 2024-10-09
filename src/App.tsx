309be7ffb90afbb8b0fcfb59f7fcba8593b0bdf4
Kauan Lima <kauanalmeidalima1405@gmail.com>
2024-10-09 20:47:21 -0300

    Melhorando o CSS da Aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
 import './App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
 
 
 function App() {
+
+
+  const [iconUrl, setIconUrl] = useState(faCopy);
+  const [textUrl, setTextUrl] = useState("Copiar");
+  
   const urlParaCopiar = 'http://localhost:8080/r/UFfsS';
 
+  {/* ESSA FUNÇÃO SERVE PRA COPIAR A URL ENCURTADA*/}
   const handleCopiarClique = () => {
     navigator.clipboard.writeText(urlParaCopiar)
       .then(() => {
-        alert('Texto copiado');
+        setIconUrl(faCheck);
+        setTextUrl("Copiado");
+
+      {/* SE FUNCIONA NÃO MEXE*/}
+        setTimeout(() => {
+          setIconUrl(iconUrl);
+          setTextUrl("Copiar");
+        }, 1000);
       })
       .catch((error) => {
         console.error('Erro ao copiar o texto: ', error);
       });
   };
-
 
   return (
     <div className='container'>
@@ -28,8 +42,8 @@ function App() {
         <p className='copiar-nova-url' onClick={handleCopiarClique}>
           <span>{urlParaCopiar}</span>
           <span style={{ display: 'flex', alignItems: 'center' }}>
-            <FontAwesomeIcon icon={faCopy} style={{ margin: '0px 5px' }} />
-            Copiar
+            <FontAwesomeIcon icon={iconUrl} style={{ margin: '0px 5px' }} />
+            {textUrl}
           </span>
         </p>
         <div className="encurta-qrcode">
