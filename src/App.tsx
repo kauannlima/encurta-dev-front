2a049c636dff3a752536a7394d4d299bd1c81749
Kauan Lima <kauanalmeidalima1405@gmail.com>
2024-10-09 22:39:43 -0300

    Adicionando QrCOde

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
 import './App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCopy } from '@fortawesome/free-solid-svg-icons';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
-import React, { useState } from 'react';
+import { faCopy, faCheck } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useTransition } from 'react';
+import axios from 'axios';
 
 
 function App() {
 
+  const localHost = "http://localhost:8080"
+  //  const localHost = "https://encurta-dev.onrender.com"
 
   const [iconUrl, setIconUrl] = useState(faCopy);
   const [textUrl, setTextUrl] = useState("Copiar");
-  
-  const urlParaCopiar = 'http://localhost:8080/r/UFfsS';
+  const [urlOriginal, setUrlOriginal] = useState('');
+  const [urlEncurtada, setUrlEncurtada] = useState('');
+  const [urlQrCode, setUrlQrCode] = useState('');
+  const [isPending, startTransition] = useTransition();
 
-  {/* ESSA FUNÇÃO SERVE PRA COPIAR A URL ENCURTADA*/}
+  const handleObterUrlOriginal = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setUrlOriginal(event.target.value);
+  };
+
+  //function para extrair urlEncurtada
+  const handleEncurtarUrlOriginal = () => {
+    startTransition(() => {
+      axios.post(`${localHost}/encurta-dev`, { urlOriginal })
+        .then((response) => {
+          setUrlEncurtada(response.data.urlEncurtada);
+          setUrlQrCode(response.data.urlQrCode)
+        })
+        .catch((error) => console.error('Erro ao buscar URL encurtada: ', error));;
+    })
+  }
+
+  {/* ESSA FUNÇÃO SERVE PRA COPIAR A URL ENCURTADA*/ }
   const handleCopiarClique = () => {
-    navigator.clipboard.writeText(urlParaCopiar)
+    navigator.clipboard.writeText(urlEncurtada)
       .then(() => {
         setIconUrl(faCheck);
         setTextUrl("Copiado");
 
-      {/* SE FUNCIONA NÃO MEXE*/}
+        {/* SE FUNCIONA NÃO MEXE*/ }
         setTimeout(() => {
           setIconUrl(iconUrl);
           setTextUrl("Copiar");
@@ -36,18 +56,25 @@ function App() {
       <h1>Encurta Dev {'{</>}'}</h1>
       <div className="encurta-body">
         <div className="encurta-url">
-          <input className='input-url' placeholder='Digite a URL original'></input>
-          <button className='button-url'>Encurtar URL</button>
+          <input className='input-url' placeholder='Digite a URL original' onChange={handleObterUrlOriginal} ></input>
+          <button className='button-url' onClick={handleEncurtarUrlOriginal}>{isPending ? 'Encurtando...' : 'Encurtar Url'}</button>
         </div>
         <p className='copiar-nova-url' onClick={handleCopiarClique}>
-          <span>{urlParaCopiar}</span>
+          <span>{urlEncurtada}</span>
           <span style={{ display: 'flex', alignItems: 'center' }}>
             <FontAwesomeIcon icon={iconUrl} style={{ margin: '0px 5px' }} />
             {textUrl}
           </span>
         </p>
         <div className="encurta-qrcode">
+          {urlQrCode != '' ? (
+            <img src={`data:image/png;base64,${urlQrCode}`} alt='QR CODE' />
+          ) : ''}
         </div>
+        <a href={`data:image/png;base64,${urlQrCode}`} download="qrcode.png" className="download-link"
+>
+          Baixar QR Code
+        </a>
       </div>
     </div>
   )
